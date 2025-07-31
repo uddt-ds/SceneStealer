aa1e920e9957a7527553e681c70f73dc4a0d326c
우다당탕 개발자 <uddt_ds@kakao.com>
2025-07-31 21:06:28 +0900

    [Feat] 컬러, 폰트 시스템 적용을 위한 UIColor, UIFont extension

---
+++ SceneStealer/Common/Extension/UIColor+Extension.swift
@@ -0,0 +1,16 @@
+//
+//  UIColor+Extension.swift
+//  SceneStealer
+//
+//  Created by Lee on 7/31/25.
+//
+
+import UIKit
+
+extension UIColor {
+    static let primaryGreen = UIColor(red: 152/255, green: 251/255, blue: 152/255, alpha: 1.0)
+    static let primaryLightGray = UIColor(red: 225/255, green: 225/255, blue: 225/255, alpha: 1.0)
+    static let primaryGray = UIColor(red: 142/255, green: 142/255, blue: 142/255, alpha: 1.0)
+    static let primaryBlack = UIColor(red: 0/255, green: 0/255, blue: 0/255, alpha: 1.0)
+    static let primaryWhite = UIColor(red: 255/255, green: 255/255, blue: 255/255, alpha: 1.0)
+}
---
+++ SceneStealer/Common/Extension/UIFont+Extension.swift
@@ -0,0 +1,32 @@
+//
+//  UIFont+Extension.swift
+//  SceneStealer
+//
+//  Created by Lee on 7/31/25.
+//
+
+import UIKit
+
+enum FontName: String {
+    case logo = "Gill Sans SemiBold Italic"
+    case header, text, sub = "Apple SD Gothic Neo"
+    case headerEngB = "Noto Sans Kannada"
+    case headerB = "Apple SD Gothic Neo Bold"
+    case eng = "Courier New"
+}
+
+extension UIFont {
+    static let logoTitle = UIFont(name: FontName.logo.rawValue, size: 20)
+    static let headerTitleLB = UIFont(name: FontName.headerB.rawValue, size: 16)
+    static let headerTitleL = UIFont(name: FontName.header.rawValue, size: 16)
+    static let headerTitleMB = UIFont(name: FontName.headerB.rawValue, size: 14)
+    static let headerTitleM = UIFont(name: FontName.header.rawValue, size: 14)
+    static let headerTitleSB = UIFont(name: FontName.headerB.rawValue, size: 12)
+    static let headerEngTitleSB = UIFont(name: FontName.headerEngB.rawValue, size: 12)
+    static let headerTitleXsB = UIFont(name: FontName.headerB.rawValue, size: 10)
+    static let textTitle = UIFont(name: FontName.text.rawValue, size: 14)
+    static let subTitleM = UIFont(name: FontName.sub.rawValue, size: 12)
+    static let subTitleS = UIFont(name: FontName.sub.rawValue, size: 10)
+    static let engTitle = UIFont(name: FontName.eng.rawValue, size: 10)
+}
+
