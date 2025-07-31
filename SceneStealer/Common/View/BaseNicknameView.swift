c7021c5965141acf27cf9308f6009cbb34dd1df3
우다당탕 개발자 <uddt_ds@kakao.com>
2025-08-01 08:01:09 +0900

    [Feat] #5 - BaseNicknameView 구현

--- SceneStealer/Common/View/BaseNicknameView.swift
+++ SceneStealer/Common/View/BaseNicknameView.swift
@@ -6,15 +6,92 @@
 //
 
 import UIKit
+import SnapKit
 
 class BaseNicknameView: UIView {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
-    }
-    */
+    let textField: UITextField = {
+        let txtField = UITextField()
+        txtField.font = .subTitleM
+        txtField.textColor = .primaryWhite
+        txtField.borderStyle = .none
+        txtField.backgroundColor = .primaryBlack
+        txtField.textAlignment = .left
+        return txtField
+    }()
 
+    private let underLineView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .primaryWhite
+        return view
+    }()
+
+    private let editButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("편집", for: .normal)
+        button.setTitleColor(.primaryWhite, for: .normal)
+        button.titleLabel?.font = .headerTitleMB
+        button.layer.cornerRadius = 20
+        button.clipsToBounds = true
+        button.layer.borderWidth = 1
+        button.layer.borderColor = UIColor.primaryWhite.cgColor
+        return button
+    }()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+
+    init(isHidden: Bool = true) {
+        super.init(frame: .zero)
+        editButton.isHidden = isHidden
+        configureHierarchy()
+        configureLayout()
+        configureView()
+    }
+
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+
+    private func configureHierarchy() {
+        [underLineView, textField, editButton].forEach { addSubview($0) }
+    }
+
+    private func configureLayout() {
+        textField.snp.makeConstraints { make in
+            make.top.equalTo(safeAreaLayoutGuide).offset(20)
+            make.directionalHorizontalEdges.equalTo(safeAreaLayoutGuide).inset(20)
+            make.height.equalTo(44)
+        }
+    
+
+        if editButton.isHidden {
+            underLineView.snp.makeConstraints { make in
+                make.top.equalTo(textField.snp.bottom)
+                make.centerX.width.equalTo(textField)
+                make.height.equalTo(1)
+            }
+        } else {
+            underLineView.snp.makeConstraints { make in
+                make.top.equalTo(textField.snp.bottom)
+                make.leading.equalTo(textField)
+                make.trailing.equalTo(textField).inset(20)
+                make.height.equalTo(1)
+            }
+        }
+
+
+        editButton.snp.makeConstraints { make in
+            make.trailing.equalTo(textField.snp.trailing)
+            make.bottom.equalTo(underLineView.snp.bottom)
+            make.width.equalTo(70)
+            make.height.equalTo(44)
+        }
+    }
+
+    private func configureView() {
+        backgroundColor = .primaryBlack
+    }
 }
