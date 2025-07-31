5357d0b7f7180ec3e7d8fe4a00215608608088c0
우다당탕 개발자 <uddt_ds@kakao.com>
2025-07-31 21:41:19 +0900

    [Feat] #4 - 버튼 공통 객체 구현

---
+++ SceneStealer/Common/View/PrimaryButton.swift
@@ -0,0 +1,35 @@
+//
+//  primaryButton.swift
+//  SceneStealer
+//
+//  Created by Lee on 7/31/25.
+//
+
+import UIKit
+
+class PrimaryButton: UIButton {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+
+    init(title: String) {
+        super.init(frame: .zero)
+        setTitle(title, for: .normal)
+        designButtonUI()
+    }
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+
+    private func designButtonUI() {
+        setTitleColor(.primaryGreen, for: .normal)
+        titleLabel?.font = .headerTitleSB
+        layer.cornerRadius = 20
+        layer.borderWidth = 2
+        layer.borderColor = UIColor.primaryGreen.cgColor
+        backgroundColor = .clear
+    }
+
+}
