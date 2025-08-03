c727d39635901f5cb1b84a1f20e853bcf8a0d30b
우다당탕 개발자 <uddt_ds@kakao.com>
2025-08-04 08:02:43 +0900

    [Feat] #14 - SplashView UI 구현

---
+++ SceneStealer/Splash/SplashVC.swift
@@ -0,0 +1,22 @@
+//
+//  SplashVC.swift
+//  SceneStealer
+//
+//  Created by Lee on 8/4/25.
+//
+
+import UIKit
+
+class SplashVC: UIViewController {
+
+    let splashView = SplashView()
+
+    override func loadView() {
+        view = splashView
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
---
+++ SceneStealer/Splash/SplashView.swift
@@ -0,0 +1,78 @@
+//
+//  SplashView.swift
+//  SceneStealer
+//
+//  Created by Lee on 8/4/25.
+//
+
+import UIKit
+
+class SplashView: UIView, InitialViewProtocol {
+
+    var userName: String? = "게스트"
+
+    let imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = .splash
+        imageView.contentMode = .scaleAspectFill
+        return imageView
+    }()
+
+    let logoLabel: UILabel = {
+        let label = UILabel()
+        label.text = "SceneStealer"
+        label.font = .logoTitle
+        label.textColor = .primaryWhite
+        label.textAlignment = .center
+        return label
+    }()
+
+    private lazy var subLabel: UILabel = {
+        let label = UILabel()
+        label.text = self.userName
+        label.font = .subTitleM
+        label.textColor = .primaryWhite
+        label.textAlignment = .center
+        label.numberOfLines = 0
+        return label
+    }()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configureHierarchy()
+        configureLayout()
+        configureView()
+    }
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+
+    func configureHierarchy() {
+        [imageView, logoLabel, subLabel].forEach { addSubview($0) }
+    }
+
+    func configureLayout() {
+        imageView.snp.makeConstraints { make in
+            make.top.equalToSuperview().offset(280)
+            make.centerX.equalToSuperview()
+            make.size.equalTo(150)
+        }
+
+        logoLabel.snp.makeConstraints { make in
+            make.top.equalTo(imageView.snp.bottom).offset(100)
+            make.centerX.equalToSuperview()
+            make.height.equalTo(36)
+        }
+
+        subLabel.snp.makeConstraints { make in
+            make.top.equalTo(logoLabel.snp.bottom).offset(20)
+            make.directionalHorizontalEdges.equalToSuperview().inset(80)
+            make.height.equalTo(32)
+        }
+    }
+
+    func configureView() {
+        backgroundColor = .primaryBlack
+    }
+}
